a72253425678a8f1c6ec334d2e76e18b9ed9e196
annu <annupawadia@gmail.com>
2024-11-07 15:36:28 +0530

    adding switch cases

--- dist/index.8cfc62b9.js
+++ dist/index.8cfc62b9.js
@@ -591,17 +591,55 @@ const initialState = {
     cartItems: [],
     wishList: []
 };
+const CART_ADD_ITEM = "cart/addItem";
+const CART_REMOVE_ITEM = "cart/removeItem";
 function reducer(state = initialState, action) {
-    console.log(action);
-    return state;
+    console.log(state);
+    switch(action.type){
+        case "cart/addItem":
+            return {
+                ...state,
+                cartItems: [
+                    ...state.cartItems,
+                    action.payload
+                ]
+            };
+    }
 }
 const store = (0, _redux.createStore)(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
 console.log(store);
 store.dispatch({
-    type: "cart/addItem",
+    type: CART_ADD_ITEM,
     payload: {
         productId: 1,
         quantity: 1
+    }
+});
+store.dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+        productId: 12,
+        quantity: 1
+    }
+});
+store.dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+        productId: 14,
+        quantity: 6
+    }
+});
+store.dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+        productId: 18,
+        quantity: 1
+    }
+});
+store.dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: {
+        productId: 1
     }
 });
 
